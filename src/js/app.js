e704515c71c403dd8deb27f9896fb8a524ae4714
Artniz23 <anizelskij@mail.ru>
2020-01-23 17:33:01 +0300

    create login project with interceptors

---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/style.css';
+import UI from './config/ui.config';
+import {validate} from './helpers/validate';
+import {showInputError, removeInputError} from './views/form';
+import {login} from './services/auth.service';
+import {notify} from './views/notifications';
+import {getNews} from './services/news.service';
+
+const {form, inputEmail, inputPassword} = UI;
+const inputs = [inputEmail, inputPassword];
+
+// Events
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onSubmit();
+})
+
+inputs.forEach((el) => {
+    el.addEventListener('focus', () => {
+        removeInputError(el);
+    })
+})
+
+// Handlers
+async function onSubmit() {
+    const isValidForm = inputs.every((el) => {
+        const isValidInput = validate(el);
+        if (!isValidInput) {
+            showInputError(el);
+        }
+        return isValidInput;
+    });
+
+    if (!isValidForm) return;
+
+    try {
+        // email - denis.m.pcspace@gmail.com
+        // pass - dmgame12345
+        await login(inputEmail.value, inputPassword.value);
+        await getNews();
+        form.reset();
+        // show notify
+        notify({msg: 'Login success', className: 'alert-success'})
+    } catch(err) {
+        // show error notify
+        notify({msg: 'Login faild', className: 'alert-danger'})
+    }
+}
